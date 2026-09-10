d5e32fd65a9e33159a54be6466b1fbe9b97c798a
ablekuka <dmitrykazov@gmail.com>
2026-09-10 09:15:39 +0300

    главное меню программы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -63,7 +63,52 @@ namespace ConsoleApp1
                     }
                 }
             }
+            while (true)
+            {
+                Console.WriteLine("\n--- МЕНЮ ---");
+                Console.WriteLine("1. Вывод данных");
+                Console.WriteLine("2. Статистика (среднее, максимальное, минимальное, сумма)");
+                Console.WriteLine("3. Сортировка по цене (пузырьковая сортировка)");
+                Console.WriteLine("4. Конвертация валюты");
+                Console.WriteLine("5. Поиск по названию");
+                Console.WriteLine("0. Выход");
+                Console.Write("Выберите пункт меню: ");
 
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        PrintData(names, prices);
+                        break;
+
+                    case "2":
+                        ShowStatistics(prices);
+                        break;
+
+                    case "3":
+                        BubbleSort(names, prices);
+                        Console.WriteLine("Сортировка успешно выполнена!");
+                        PrintData(names, prices);
+                        break;
+
+                    case "4":
+                        ConvertCurrency(prices);
+                        break;
+
+                    case "5":
+                        SearchByName(names, prices);
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Программа завершена. До свидания!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Неверный пункт меню. Попробуйте еще раз.");
+                        break;
+                }
+            }
         }
     }
 }
