38d5fbc278a0059230c2ad389c2768aa06bcd575
ablekuka <dmitrykazov@gmail.com>
2026-09-10 09:13:30 +0300

    ввод кол-ва операций и инициализация массива

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            while (true)
+            {
+                Console.Write("Введите количество операций (от 2 до 40): ");
+                if (int.TryParse(Console.ReadLine(), out count) && count >= 2 && count <= 40)
+                {
+                    break;
+                }
+                Console.WriteLine("Ошибка! Нужно ввести целое число от 2 до 40.");
+            }
+
+            string[] names = new string[count];
+            double[] prices = new double[count];
         }
     }
 }
